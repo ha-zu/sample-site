e33065abd13be2f83d89b53d82d31616680c0ef2
ha-zu <nagaoka041.work@gmail.com>
2023-09-09 04:16:05 +0900

    add member detail, modified display

---
+++ src/features/member/components/MemberDetail.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent } from "react";
+
+interface Props {
+  className?: string;
+}
+
+export const MemberDetail: FunctionComponent<Props> = ({ className }) => {
+  return (
+    <div>
+      <div>MemberDetail</div>
+    </div>
+  );
+};
--- src/features/member/components/MemberLists.tsx
+++ src/features/member/components/MemberLists.tsx
@@ -5,32 +5,38 @@ const Members = [
   // example
   {
     image: "https://via.placeholder.com/250",
-    name: "メンバー1",
+    nameJA: "メンバー1",
+    nameUS: "Member1",
     departments: ["代表取締役"],
   },
   {
     image: "https://via.placeholder.com/250",
-    name: "メンバー2",
+    nameJA: "メンバー2",
+    nameUS: "Member2",
     departments: ["役員", "営業"],
   },
   {
     image: "https://via.placeholder.com/250",
-    name: "メンバー3",
+    nameJA: "メンバー3",
+    nameUS: "Member3",
     departments: ["役員", "経営企画"],
   },
   {
     image: "https://via.placeholder.com/250",
-    name: "メンバー4",
+    nameJA: "メンバー4",
+    nameUS: "Member4",
     departments: ["イベント"],
   },
   {
     image: "https://via.placeholder.com/250",
-    name: "メンバー5",
+    nameJA: "メンバー5",
+    nameUS: "Member5",
     departments: ["広報"],
   },
   {
     image: "https://via.placeholder.com/250",
-    name: "メンバー6",
+    nameJA: "メンバー6",
+    nameUS: "Member6",
     departments: ["経理"],
   },
 ];
@@ -39,23 +45,28 @@ export const MemberLists: FunctionComponent = ({}) => {
   return (
     <div className="grid grid-cols-3 gap-3">
       {Members.map((member, member_index) => (
-        <div className="w-[270px] h-[350px] bg-gray-100" key={member_index}>
+        <div className="w-[280px] h-[390px] bg-gray-100" key={member_index}>
           <a href="#">
-            <Image src={member.image} width={270} height={250} alt="img" />
+            <Image src={member.image} width={280} height={280} alt="img" />
+            <div className="flex justify-center">
+              <div className="p-2">
+                <p>
+                  <strong>{member.nameJA}</strong>
+                </p>
+                <p>{member.nameUS}</p>
+              </div>
+            </div>
+            <div className="flex justify-center p-1">
+              {member.departments.map((department, department_index) => (
+                <span
+                  className="m-1 p-1 rounded bg-blue-300"
+                  key={department_index}
+                >
+                  {department}
+                </span>
+              ))}
+            </div>
           </a>
-          <div className="flex justify-center">
-            <p>{member.name}</p>
-          </div>
-          <div className="flex justify-center p-2">
-            {member.departments.map((department, department_index) => (
-              <span
-                className="m-1 p-1 rounded bg-blue-300"
-                key={department_index}
-              >
-                {department}
-              </span>
-            ))}
-          </div>
         </div>
       ))}
     </div>
